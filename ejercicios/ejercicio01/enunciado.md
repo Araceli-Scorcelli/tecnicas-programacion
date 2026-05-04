a9a12679fbf558517c4e8cd5a6e4006eed2cd86c
Araceli Scorcelli <aracelixgj@gmail.com>
2026-05-04 11:55:28 -0300

    Comentar solución del ejercicio 01

---
+++ ejercicios/ejercicio01/enunciado.md
@@ -0,0 +1,20 @@
+Ejercicio 1 — Clasificación de temperatura 
+
+Una estación meteorológica registra una temperatura ingresada por el usuario.
+
+El programa debe:
+
+- pedir una temperatura (número entero)
+- indicar si es:
+    - bajo cero (menor a 0)
+    - templada (entre 0 y 25)
+    - alta (mayor a 25)
+
+ Extensión: validar que el dato ingresado sea numérico.
+
+<
+>
+<=
+>=
+==
+!=  # También puede ir al final
--- ejercicios/ejercicio01/solucion.py
+++ ejercicios/ejercicio01/solucion.py
@@ -1,1 +1,24 @@
-print("Hola, Técnicas de Programación")
+# Ejercicio 1 - Clasificación de temperatura
+# El programa pide una temperatura al usuario y la clasifica según su valor.
+
+try:
+    # input() pide un dato al usuario.
+    # int() convierte ese dato a número entero.
+    temperatura = int(input("Ingrese una temperatura: "))
+
+    # Si la temperatura es menor que 0, se considera bajo cero.
+    if temperatura < 0:
+        print("La temperatura está bajo cero.")
+
+    # Esta condición evalúa si la temperatura está dentro del rango 0 a 25 inclusive.
+    # Es decir: temperatura debe ser mayor o igual a 0 y menor o igual a 25.    
+    elif 0 <= temperatura <= 25:
+        print("La temperatura es templada.")
+  
+   # Si no cumple las condiciones anteriores, entonces necesariamente es mayor a 25.
+    else:
+        print("La temperatura es alta.")
+
+# Si el usuario ingresa algo que no puede convertirse a entero, se captura el error.
+except ValueError:
+    print("Error: debe ingresar un número entero.")
